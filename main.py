101e9481d5587df679b0b2b8573efa8272e841b3
hasanmalik825-gh <gclab825@gmail.com>
2024-11-08 15:22:37 +0200

    basic api

---
+++ main.py
@@ -0,0 +1,31 @@
+# Import FastAPI and necessary packages
+from fastapi import FastAPI,Query
+import uvicorn
+import logging
+# Initialize FastAPI app
+app = FastAPI()
+
+@app.get("/")
+async def index() -> dict:
+    """
+    Index route to check if the API is working.
+    Returns:
+        dict: The status of the API.
+    """
+    logging.info("Index is working")
+    return {
+        "Status": True,
+        "message": "API is working.."
+    }
+
+@app.post("/welcome")
+async def welcome_message(
+    name: str = Query(..., description="Enter your name"),
+) -> dict:
+    
+    return {
+        "message":f"Hello {name}"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8800)
